91e042e29a81693b2178eb33b169b79bee8425b1
Luiz Feranando Lucio Araujo <99514007+LuizFernando-1@users.noreply.github.com>
2022-10-28 20:11:57 -0300

    Update index.js

--- aula06/index.js
+++ aula06/index.js
@@ -1,5 +1,7 @@
-console.log('joão nasceu em 1984;')
-console.log('Em 2000 joão conheceu maria')
-console.log('jo[')
-console.log('')
-console.log('')
+let nome = 'João'
+
+console.log(nome, 'nasceu em 1984;')
+console.log('Em 2000', nome,'conheceu maria')
+console.log(nome, 'casou-se com maria em 2012')
+console.log('maria teve um filho com', nome, 'em 2015')
+console.log('O filho de' ,nome, 'se chama Eduardo')
