816ad64be48cfbb44c323d376c6e0fed1ddfcd81
Luiz Feranando Lucio Araujo <99514007+LuizFernando-1@users.noreply.github.com>
2022-11-16 12:37:32 -0300

    aula 16

---
+++ aula16/index.js
@@ -0,0 +1,31 @@
+let alunos = ['Luiz', 'Maria', 'João']
+alunos = 12345
+
+
+console.log(typeof alunos)
+console.log(alunos instanceof Array)
+
+
+
+//console.log(alunos.slice(0, -2))   
+
+//delete alunos[1] Deleta o elemento deixando o índice vazio  
+
+//const removido = alunos.shift() // Remove o de trás
+//const removido = alunos.pop()  // Remove o da frente
+
+//alunos.unshift('Tai')  //Adiciona no começo
+//alunos.unshift('Fábio')
+
+//alunos.push('Julio')  // Adiciona no fim do array
+
+//alunos[alunos.length] = 'Luiza'  // Adiciona no fim do array
+//alunos[alunos.length] = 'Mariana'
+//alunos[alunos.length] = 'Esther'
+
+//alunos[0] = 'Pedro'  // Altera 
+//alunos[3] = 'Lucas' // Adiciona
+
+//console.log(alunos)   
+//console.log(alunos[0])
+//console.log(alunos[2])
