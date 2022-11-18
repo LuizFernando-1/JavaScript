f9c52c5685fed5c0a7a5a7189520503eb2d6cf7d
Luiz Feranando Lucio Araujo <99514007+LuizFernando-1@users.noreply.github.com>
2022-11-18 20:16:33 -0300

    aula 23 - Avaliação de Curto-Circuito (Short-Circuit)

---
+++ aula23/index.js
@@ -0,0 +1,24 @@
+/*
+FALSY
+0
+' '  " " ` `
+null / undefined
+NaN
+false
+*/
+
+const corUsuario = 'vermelho'
+const corPadrao = corUsuario || 'preto'
+
+console.log(corPadrao)
+
+
+
+
+/*function falaOi () {
+    return 'Oi'
+}
+
+const vaiExecutar = 1
+
+console.log(vaiExecutar && falaOi()) */
