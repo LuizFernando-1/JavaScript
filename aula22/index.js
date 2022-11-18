fbf956bb5a8d1722b580fe5299e0547e75719e6e
Luiz Feranando Lucio Araujo <99514007+LuizFernando-1@users.noreply.github.com>
2022-11-18 19:50:32 -0300

    aula22 - operadores lógicos

---
+++ aula22/index.js
@@ -0,0 +1,15 @@
+/*
+Operadores lógicos
+&& -> and -> e //Todas as expressões precisam ser verdadeiras para retornar true
+|| -> or -> ou //Uma das expressões precisa ser true para retornar true
+! -> not -> não
+*/
+
+/*
+const usuario = 'Luiz'
+const senha = '123456'
+
+const vaiLogar = usuario === 'Luiz' && senha === '12345'
+*/
+
+console.log(!true) 
