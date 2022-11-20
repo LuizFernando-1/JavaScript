9c0d04b4137c64bea603eba22d3b8811beb8bcd0
Luiz Feranando Lucio Araujo <99514007+LuizFernando-1@users.noreply.github.com>
2022-11-20 19:10:43 -0300

    aula24 else if part 1

---
+++ aula24/index.js
@@ -0,0 +1,11 @@
+var hora = 18
+
+if (hora >= 0 && hora <= 11) {
+    console.log('Bom dia')
+} else if (hora >= 12 && hora <= 17) {
+    console.log('Boa Tarde')
+} else if (hora >= 18 && hora <= 23) {
+    console.log('Boa Noite')
+} else {
+    console.log('Olá')
+}
